68a7c559dc69a1320b6ca180e1f7dcce0880990e
Stabbar <User2febus@gmail.com>
2022-11-08 23:57:28 +0300

    replaced

--- json_tokener.c
+++ json_tokener.c
@@ -630,8 +630,6 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 
 		case json_tokener_state_escape_unicode:
 		{
-			unsigned int got_hi_surrogate = 0;
-
 			/* Handle a 4-byte sequence, or two sequences if a surrogate pair */
 			while (1)
 			{
@@ -643,14 +641,24 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 					{
 						unsigned char unescaped_utf[4];
 
-						if (got_hi_surrogate)
+						if (tok->got_hi_surrogate)
 						{
 							if (IS_LOW_SURROGATE(tok->ucs_char))
 							{
+								/* remove the utf8_replacement_char */
+								/* which may generate during */
+								/* parsing the high surrogate pair. */
+								if (!strcmp(
+								        tok->pb->buf,
+								        (char *)
+								            utf8_replacement_char))
+								{
+									printbuf_reset(tok->pb);
+								}
 								/* Recalculate the ucs_char, then fall thru to process normally */
 								tok->ucs_char =
 								    DECODE_SURROGATE_PAIR(
-								        got_hi_surrogate,
+								        tok->got_hi_surrogate,
 								        tok->ucs_char);
 							}
 							else
@@ -662,7 +670,7 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 								    (char *)utf8_replacement_char,
 								    3);
 							}
-							got_hi_surrogate = 0;
+							tok->got_hi_surrogate = 0;
 						}
 
 						if (tok->ucs_char < 0x80)
@@ -686,7 +694,7 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 							 * the beginning of another sequence, which
 							 * should be the low surrogate.
 							 */
-							got_hi_surrogate = tok->ucs_char;
+							tok->got_hi_surrogate = tok->ucs_char;
 							/* Not at end, and the next two chars should be "\u" */
 							if ((len == -1 ||
 							     len > (tok->char_offset + 2)) &&
@@ -717,6 +725,8 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 									    (char *)
 									        utf8_replacement_char,
 									    3);
+									tok->ucs_char = 0;
+									tok->st_pos = 0;
 									goto out;
 								}
 								tok->ucs_char = 0;
@@ -786,7 +796,8 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 				if (!ADVANCE_CHAR(str, tok) || !PEEK_CHAR(c, tok))
 				{
 					/* Clean up any pending chars */
-					if (got_hi_surrogate)
+					if (tok->got_hi_surrogate &&
+					    strcmp(tok->pb->buf, (char *)utf8_replacement_char))
 						printbuf_memappend_fast(
 						    tok->pb, (char *)utf8_replacement_char, 3);
 					goto out;
@@ -964,6 +975,9 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 		case json_tokener_state_array:
 			if (c == ']')
 			{
+				// Minimize memory usage; assume parsed objs are unlikely to be changed
+				json_object_array_shrink(current, 0);
+
 				if (state == json_tokener_state_array_after_sep &&
 				    (tok->flags & JSON_TOKENER_STRICT))
 				{
@@ -997,6 +1011,9 @@ struct json_object *json_tokener_parse_ex(struct json_tokener *tok, const char *
 		case json_tokener_state_array_sep:
 			if (c == ']')
 			{
+				// Minimize memory usage; assume parsed objs are unlikely to be changed
+				json_object_array_shrink(current, 0);
+
 				saved_state = json_tokener_state_finish;
 				state = json_tokener_state_eatws;
 			}
